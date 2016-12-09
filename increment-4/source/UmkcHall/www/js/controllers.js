940c5f58b3c087e15383aa5e53ff666e41d8ad4e
nagarajureddymyaka <nmcyb@mail.umkc.edu>
2016-12-08 21:09:26 -0600

    added

--- increment-4/source/UmkcHall/www/js/controllers.js
+++ increment-4/source/UmkcHall/www/js/controllers.js
@@ -384,6 +384,50 @@ angular.module('starter.controllers',['ngResource','ngCordova'])
 
 
     }
+    // //Cordova SMS
+    //
+    //   document.addEventListener("deviceready", function () {
+    //
+    //     $cordovaSms
+    //       .send('phonenumber', 'SMS content', options)
+    //       .then(function() {
+    //         // Success! SMS was sent
+    //       }, function(error) {
+    //         // An error occurred
+    //       });
+    //
+    //   });
+    //
+    // });
+
+     // Email Composer
+  //   $cordovaEmailComposer.isAvailable().then(function() {
+  //     // is available
+  //   }, function () {
+  //     // not available
+  //   });
+  //
+  //   var email = {
+  //     to: 'max@mustermann.de',
+  //     cc: 'erika@mustermann.de',
+  //     bcc: ['john@doe.com', 'jane@doe.com'],
+  //     attachments: [
+  //       'file://img/logo.png',
+  //       'res://icon.png',
+  //       'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
+  //       'file://README.pdf'
+  //     ],
+  //     subject: 'Cordova Icons',
+  //     body: 'How are you? Nice greetings from Leipzig',
+  //     isHtml: true
+  //   };
+  //
+  //   $cordovaEmailComposer.open(email).then(null, function () {
+  //     // user cancelled email
+  //   });
+  // });
+
+
   })
 
 
@@ -505,6 +549,50 @@ angular.module('starter.controllers',['ngResource','ngCordova'])
 
 
     }
+
+
+    // //Cordova SMS
+    //
+    //   document.addEventListener("deviceready", function () {
+    //
+    //     $cordovaSms
+    //       .send('phonenumber', 'SMS content', options)
+    //       .then(function() {
+    //         // Success! SMS was sent
+    //       }, function(error) {
+    //         // An error occurred
+    //       });
+    //
+    //   });
+    //
+    // });
+
+    // Email Composer
+    //   $cordovaEmailComposer.isAvailable().then(function() {
+    //     // is available
+    //   }, function () {
+    //     // not available
+    //   });
+    //
+    //   var email = {
+    //     to: 'max@mustermann.de',
+    //     cc: 'erika@mustermann.de',
+    //     bcc: ['john@doe.com', 'jane@doe.com'],
+    //     attachments: [
+    //       'file://img/logo.png',
+    //       'res://icon.png',
+    //       'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
+    //       'file://README.pdf'
+    //     ],
+    //     subject: 'Cordova Icons',
+    //     body: 'How are you? Nice greetings from Leipzig',
+    //     isHtml: true
+    //   };
+    //
+    //   $cordovaEmailComposer.open(email).then(null, function () {
+    //     // user cancelled email
+    //   });
+    // });
   })
 
 //End Of Royal Hall
